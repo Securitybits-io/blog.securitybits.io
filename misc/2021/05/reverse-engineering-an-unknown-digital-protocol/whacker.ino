2abcde462f101b548e4299c1f7c457ebb7bc5271
Securitybits-io <Securitybits-io@users.noreply.github.com>
2021-05-22 07:34:28 +0000

    Automated deployment: Sat May 22 07:34:28 UTC 2021 5eca3183168858d783499ef1d15157c35758aa23

--- misc/2021/05/reverse-engineering-an-unknown-digital-protocol/whacker.ino
+++ misc/2021/05/reverse-engineering-an-unknown-digital-protocol/whacker.ino
@@ -23,22 +23,22 @@ String pins_used[12] = {"[ ]","[ ]","[ ]","[ ]","[ ]","[ ]","[ ]","[ ]","[ ]","[
 int candidates[6] = {2, 2, 2, 2, 2, 2};
 int pins[6] = {0, 0, 0, 0, 0, 0};
 
-SoftwareSerial ser(SER_RX, SER_TX);
+SoftwareSerial target(SER_RX, SER_TX);
 
 void setup() {
   Serial.begin(BAUDRATE);
-  
+
   pinMode(PIN_READ, INPUT);
   pinMode(PIN_MONITOR, OUTPUT);
   digitalWrite(PIN_MONITOR, LOW);
-  
+
   for (int i=2;i<MAXPINS+1;i++){
     pinMode(i, OUTPUT);
     digitalWrite(i, LOW);
     }
 
   Serial.println("Ready");
-  ser.begin(19200);
+  target.begin(19200);
 }
 
 void set_used_pins(int pin){
@@ -47,7 +47,7 @@ void set_used_pins(int pin){
   }
 
 void print_status(){
-  for (int i=0;i<2;i++) Serial.println("\r");  
+  for (int i=0;i<2;i++) Serial.println("\r");
   Serial.print("Used Pins: ");
   for (int i=0;i<12;i++){
     Serial.print(pins_used[i]);
@@ -59,12 +59,12 @@ void print_status(){
   Serial.print("| Level: ");
   Serial.println(current_level);
   Serial.print("Pins : ");
-  
+
   for (int i=0;i<6;i++){
     Serial.print(pins[i]);
     Serial.print(" ");
     }
-  
+
   Serial.print(" | Top: ");
   Serial.print(top_level);
   Serial.println("/50");
@@ -92,7 +92,7 @@ void hit(){
         set_used_pins(candidate);
         pins[peak_state] = candidate;
       }
-    print_status(); 
+    print_status();
     current_level++;
     if (current_level > top_level)
       top_level++;
@@ -100,44 +100,45 @@ void hit(){
 
 void miss(){
     candidates[peak_state] = candidates[peak_state] + 1;
-    
+
     if (candidates[peak_state] > 12){
       Serial.println("INVALID PIN.... Resetting to 2");
       candidates[peak_state] = 2;
     }
-    
+
     print_status();
     current_level = 0;
   }
 
 void loop() {
-  while(ser.available()){
-    inData = ser.read();
+  while(target.available()){
+    inData = target.read();
     strBuf += inData;
     }
-    
+
   if (strBuf.endsWith(F("\n"))){
     strBuf.trim();
     if (strBuf.length() > 0) { str = strBuf; }
     strBuf = "";
     }
-    
-  if (str.indexOf(F("You missed it")) >= 0) {
+
+  if (str.indexOf(F("missed")) >= 0) {
     miss();
     str = "";
-    ser.write("\r");
-  
-  } else if (str.indexOf(F("You whacked it")) >= 0) {
+    target.write("\r");
+
+  } else if (str.indexOf(F("whacked")) >= 0) {
     hit();
-    str = "";     
-    
-  } else if (str.indexOf(F(" please step into the yard by pressing")) >= 0) {
+    str = "";
+
+  } else if (str.indexOf(F("When you are ready")) >= 0) {
     Serial.println("Start the game");
+    delay(500);
     print_status();
     str = "";
-    ser.write("\r");
+    target.write("\r");
   }
-  
+
   int value = digitalRead(PIN_READ);
   if (value != state){
     state = value;
@@ -146,8 +147,8 @@ void loop() {
       peak_count++;
       }
     }
-    
-  if (peak_start && peak_start + 103 < millis() && state == 0){
+
+  if (state == 0 && peak_start && peak_start + 100 < millis()){
     peak_state = peak_count - 1;
     whack(peak_state);
     peak_start = 0;
